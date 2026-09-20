fe4a90e2422012d223ed3eba51def534c16c2d39
Hayden Gamble <hayden-gamble@uiowa.edu>
2026-09-19 21:08:36 -0500

    modify: modifed the scripting files for fadom, info, and index to follow common naming conventions

--- js/details.js
+++ js/details.js
@@ -3,10 +3,12 @@ import { loadConfig } from './core/config.js';
 import { getSupabaseClient } from './core/supabase.js';
 import { normalizeOpenLibraryId } from './core/media.js';
 
-// Global vars
+// Load configuration and initialize Supabase client
 let PROXY_URL = '';
-const params = new URLSearchParams(window.location.search);
 let supabaseClient = null;
+
+// Global vars
+const params = new URLSearchParams(window.location.search);
 let globalData = null;
 let tvmazeEpisodesMap = {};
 let fullCastData = [];
@@ -14,7 +16,7 @@ let fullCrewData = [];
 let directorData = null;
 let allLogsData = [];
 
-// 
+// Get params
 const id = params.get('id');
 const type = params.get('type');
 
